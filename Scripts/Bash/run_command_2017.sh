975e591306a01fdc7b924dcc94ef7d4b12056b79
Fábio Vital <fabio.vital@aptoide.com>
2018-09-03 11:52:01 +0100

    After build copy dll to bds and asf repositories

---
+++ Scripts/Bash/run_command_2017.sh
@@ -0,0 +1,4 @@
+mkdir -p ../bds-unity-dll/dependencies/2017/
+mkdir -p ../asf-unity-dll/dependencies/2017/
+cp ./appcoins-unity-support-2017/bin/Debug/appcoins-unity-support-2017.dll ../bds-unity-dll/dependencies/2017/
+cp ./appcoins-unity-support-2017/bin/Debug/appcoins-unity-support-2017.dll ../afs-unity-dll/dependencies/2017/

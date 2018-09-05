ca04f83a2d0e267ada19db657e7ec5337886a6da
Fábio Vital <fabio.vital@aptoide.com>
2018-09-05 09:43:14 +0100

    fix: bash commands (copy unity support dll to asf and bds)

--- Scripts/Bash/run_command_5_6.sh
+++ Scripts/Bash/run_command_5_6.sh
@@ -1,4 +1,4 @@
 mkdir -p ../bds-unity-dll/dependencies/5.6/
 mkdir -p ../asf-unity-dll/dependencies/5.6/
-cp ./appcoins-unity-support-5.6/bin/Debug/AppcoinsUnitySupport5_6.dll ../bds-unity-dll/dependencies/5.6/
-cp ./appcoins-unity-support-5.6/bin/Debug/AppcoinsUnitySupport5_6.dll ../asf-unity-dll/dependencies/5.6/
+cp ./appcoins-unity-support-5.6/bin/Debug/AppCoinsUnitySupport5_6.dll ../bds-unity-dll/dependencies/5.6/
+cp ./appcoins-unity-support-5.6/bin/Debug/AppCoinsUnitySupport5_6.dll ../asf-unity-dll/dependencies/5.6/

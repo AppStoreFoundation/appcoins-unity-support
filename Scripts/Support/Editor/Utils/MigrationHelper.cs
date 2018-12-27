731a4b7c8103d8392616fea9eab70366fd42641c
Nuno Monteiro <nuno.monteir@gmail.com>
2018-12-27 11:59:42 +0000

    Added more migration rules and fixed bugs that prevented deletion

--- Scripts/Support/Editor/Utils/MigrationHelper.cs
+++ Scripts/Support/Editor/Utils/MigrationHelper.cs
@@ -8,8 +8,23 @@ public static class MigrationHelper {
 
         Debug.Log("Cleaning up possible old plugin files");
 
+        //v0.3 Files
         string scriptsPath = Application.dataPath + "/AppcoinsUnity";
-        string editorScriptsPath = scriptsPath + "/Scripts/Editor";
+        string editorScriptsPath = scriptsPath + "/Editor";
+
+        string[] olderUnusedFiles03 = { 
+            "unity-2018.pdb"
+             };
+
+        DeleteFiles(scriptsPath, olderUnusedFiles03);
+
+        string[] olderUnusedFiles = {
+            "unity-editor-2018.pdb",
+            "unity-editor-2018.dll",
+            "unity-editor-2018.dll.mdb"
+             };
+
+        DeleteFiles(editorScriptsPath, olderUnusedFiles);
 
         //string[] olderScriptFiles = { "AppcoinsPurchaser", "AppcoinsSku",
             //"AppcoinsUnity", "BashUtils", 
@@ -20,21 +35,32 @@ public static class MigrationHelper {
             //"AppCoinsUnityPluginEditorMode5_6",
             //"AppCoinsUnityPluginEditorMode2017" };
 
-        string[] olderEditorScriptFiles = { "appcoins-unity-support-2018",
-            "appcoins-unity-support-5_6", "appcoins-unity-support-2017",
-            "AppCoinsUnitySupport2018", "AppCoinsUnitySupport2017",
-            "AppCoinsUnitySupport5_6" };
+        editorScriptsPath = scriptsPath + "/Scripts/Editor";
+
+        //NOTE:works with partial file names but be careful to avoid unwanted deletions!
+        string[] olderEditorScriptFiles = { 
+            "appcoins-unity-support-2018.dll",
+            "appcoins-unity-support-5_6.dll",
+            "appcoins-unity-support-2017.dll",
+            "AppCoinsUnitySupport2018.dll",
+            "AppCoinsUnitySupport2017.dll",
+            "AppCoinsUnitySupport5_6.dll" };
 
         //DeleteFiles(scriptsPath, olderScriptFiles);
         DeleteFiles(editorScriptsPath, olderEditorScriptFiles);
     }
 
     private static void DeleteFiles(string dirPath, string[] filesToDelete) {
-        foreach(string filePath in Directory.GetFiles(dirPath))
+        //If folder doesnt exist return immediately
+        if (!Directory.Exists(dirPath))
+            return;
+
+        string[] files = Directory.GetFiles(dirPath);
+        foreach(string filePath in files)
         {
             string fName = Path.GetFileName(filePath);
 
-            if (Array.Exists(filesToDelete, dF => dF.Equals(fName)))
+            if (Array.Exists(filesToDelete, dF => fName.Contains(dF)))
             {
                 File.Delete(filePath);
             }

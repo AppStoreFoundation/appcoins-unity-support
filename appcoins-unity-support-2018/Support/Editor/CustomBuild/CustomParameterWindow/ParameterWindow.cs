f27ec9d2cd6b5dee859b0204711cdbb2112da677
Mikie Ribeiro <mikie.ribeiro@aptoide.com>
2019-01-11 09:51:11 +0000

    Parameter Window
    -Created a new parameter window

---
+++ appcoins-unity-support-2018/Support/Editor/CustomBuild/CustomParameterWindow/ParameterWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+
+public class ParameterWindow : EditorWindow
+{
+    string developerWalletAddress;
+    string developerBDSPublicKey;
+    bool shouldLog;
+    bool useAdsSDK = true;
+    bool groupEnabled;
+    //float myFloat = 1.23f;
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        developerWalletAddress = EditorGUILayout.TextField("Developer Wallet Adress", "");
+        developerBDSPublicKey = EditorGUILayout.TextField("Developer BDS Public Key", "");
+
+
+        shouldLog = EditorGUILayout.Toggle("Should Log", shouldLog);
+        useAdsSDK = EditorGUILayout.Toggle("Use Ads SDK", useAdsSDK);
+        EditorGUILayout.EndToggleGroup();
+        //myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+
+    }
+}
